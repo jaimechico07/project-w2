b37abfe6dadb8e3a2e9207e7cc249ad081faeaa8
jaime chico loayza <jrchldev@gmail.com>
2024-08-07 13:23:05 -0500

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   spotify: {
     clientId: '0fa0fc7cbede46569872d551a394c103',
     clientSecret: 'c30ad7be33314b2ea52507fb63afa46d',
-    redirectUri: 'https://cookinglearn.app/callback',
+    redirectUri: 'http://localhost:4200/callback',
     authUrl: 'https://accounts.spotify.com/authorize',
     tokenUrl: 'https://accounts.spotify.com/api/token'
   }
