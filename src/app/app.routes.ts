be8f009ce7de077f59a516aeead7d774cb11fe81
jaime chico loayza <jrchldev@gmail.com>
2024-08-07 12:37:15 -0500

    vercel routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes} from '@angular/router';
-// import { authGuard } from './auth/guards/auth.guard';
+import { authGuard } from './auth/guards/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { BudgetComponent } from './pages/budget/budget.component';
 import { BudgetRouterComponent } from './pages/budget-router/budget-router.component';
@@ -11,7 +11,7 @@ import { SpotifyPlayListDetailComponent } from './pages/cooking-music/spotify/sp
 
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { MassCalculatorComponent } from './pages/mass-calculator/mass-calculator.component';
-// import { CallbackComponent } from './auth/services/spotify/callback.component'
+import { CallbackComponent } from './auth/services/spotify/callback.component'
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -26,20 +26,20 @@ export const routes: Routes = [
   { path: '', redirectTo: "login", pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'BudgetComponent', component: BudgetComponent  },
-  { path: 'BudgetRouterComponent/:monto', component: BudgetRouterComponent  },
-  { path: 'Masscalculator', component: MassCalculatorComponent },
-  { path: 'recipes', component: RecipesComponent },
-  { path: 'cookingMusic', component: CookingMusicComponent },
-  { path: 'timer', component: TimerComponent },
-  { path: 'myrecipes', component: MyRecipesComponent },
-  { path: 'myrecipesdetail/:userId/:id', component: DetailMyrecipesComponent },
-  { path: 'todolist', component: TodolistComponent },
-  // { path: 'callback', component: CallbackComponent },
-  { path: 'spotifyPlayListDetail/:id', component: SpotifyPlayListDetailComponent },
-  { path: 'mealDetail/:id', component: MealDetailComponent },
-  { path: 'mealListCategories/:strCategory', component: MealListCategoriesComponent },
+  { path: 'home', component: HomeComponent,canActivate: [authGuard] },
+  { path: 'BudgetComponent', component: BudgetComponent,canActivate: [authGuard]  },
+  { path: 'BudgetRouterComponent/:monto', component: BudgetRouterComponent,canActivate: [authGuard]  },
+  { path: 'Masscalculator', component: MassCalculatorComponent,canActivate: [authGuard] },
+  { path: 'recipes', component: RecipesComponent,canActivate: [authGuard] },
+  { path: 'cookingMusic', component: CookingMusicComponent,canActivate: [authGuard] },
+  { path: 'timer', component: TimerComponent,canActivate: [authGuard] },
+  { path: 'myrecipes', component: MyRecipesComponent,canActivate: [authGuard] },
+  { path: 'myrecipesdetail/:userId/:id', component: DetailMyrecipesComponent,canActivate: [authGuard] },
+  { path: 'todolist', component: TodolistComponent,canActivate: [authGuard] },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'spotifyPlayListDetail/:id', component: SpotifyPlayListDetailComponent,canActivate: [authGuard] },
+  { path: 'mealDetail/:id', component: MealDetailComponent,canActivate: [authGuard] },
+  { path: 'mealListCategories/:strCategory', component: MealListCategoriesComponent,canActivate: [authGuard] },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'not-found' },
 ];
